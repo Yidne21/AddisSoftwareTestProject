2c70c9d7cfb042870c7fe98bc6f7a4c5389e4511
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-12-04 20:34:12 +0300

    change the api url to mockend

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+//uncomment This code when using it on your lacal Machine 
+/** 
 export const loadUsersApi = async () =>
   await axios.get("http://localhost:3500/users");
 
@@ -11,3 +13,18 @@ export const deleteUserApi = async (userId) =>
 
 export const updateUserApi = async (userId, userInfo) =>
   await axios.put(`http://localhost:3500/users/${userId}`, userInfo);
+
+*/
+
+//Comment those below code when using it on your local Machine
+export const loadUsersApi = async () =>
+  await axios.get("https://mockend.com/Yidne21/api_server/Users");
+
+export const creaeteUserApi = async (user) =>
+  await axios.post("https://mockend.com/Yidne21/api_server/Users", user);
+
+export const deleteUserApi = async (userId) =>
+  await axios.delete(`https://mockend.com/Yidne21/api_server/Users/${userId}`);
+
+export const updateUserApi = async (userId, userInfo) =>
+  await axios.put(`https://mockend.com/Yidne21/api_server/Users/${userId}`, userInfo);
